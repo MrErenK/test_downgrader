d5aefa344e79966e93c8cf8f71dfa177ace03bf6
MrErenK <akbaseren4751@gmail.com>
2026-02-28 17:38:35 +0300

    fix

--- sources/GUI/MainForm.Designer.cs
+++ sources/GUI/MainForm.Designer.cs
@@ -388,8 +388,8 @@ namespace JetpackDowngraderGUI
             //
             // MainForm
             //
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
             this.ClientSize = new System.Drawing.Size(859, 403);
             this.Controls.Add(this.DSPanel);
