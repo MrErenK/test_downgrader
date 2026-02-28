e4da0449df31817152d09ec903c53f15021449bf
MrErenK <akbaseren4751@gmail.com>
2026-02-28 17:54:05 +0300

    try to fix dpi

--- sources/GUI/Classes/Program.cs
+++ sources/GUI/Classes/Program.cs
@@ -6,6 +6,7 @@ namespace JetpackDowngraderGUI
     {
         public static void Main(string[] args)
         {
+            Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
--- sources/GUI/MainForm.Designer.cs
+++ sources/GUI/MainForm.Designer.cs
@@ -420,8 +420,8 @@ namespace JetpackDowngraderGUI
             //
             // MainForm
             //
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
             this.ClientSize = new System.Drawing.Size(859, 403);
             this.Controls.Add(this.DSPanel);
